57024ba970d762d747efee0c7e8e17c5d14f3ad9
Елена Григорьева <elenagrigoreva@MacBook-Pro-Elena.local>
2022-08-26 18:22:36 +0300

    Решение задачи 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,51 +1,39 @@
-﻿// Задайте массив заполненный случайными положительными трехзначными числами. Напишите программу, которая покажет количество четных 
-// чисел в массиве. 
+﻿// Задайте массив заполненный случайными положительными трехзначными числами. 
+// Напишите программу, которая покажет количество четных чисел в массиве. 
 // [345, 897, 568, 234] -> 2
 
 
-// int[] numbers = new int[10];
-// FillArrayRandomNumbres(numbers);
-// PrintArray(numbers);
-// int oddNumbersCount = 0;
-// int evenNumbersCount = 0;
-// // int limit = int.Parse(console.ReadLine());
+int[] numbers = new int[10];
+FillArrayRandomNumbres(numbers);
+PrintArray(numbers);
+CalculateEven(numbers);
 
-// void FillArrayRandomNumbres(int[] array)
-// {
-// for (int i = 0; i < array.Length; i++)
-// {
-// array[i] = new Random().Next(100, 1000);
-// }
-// }
+void FillArrayRandomNumbres(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
 
-// void PrintArray(int[] array)
-// {
-// for(int i = 0; i < array.Length; i++)
-// {
-// Console.Write(array[i] + " ");
-// }
-// Console.WriteLine();
-// }
-// // while (array[i] < array.Length)
-// // while (array[i] <= limit)
-// {
-// if (evenNumbersCount % 2 == 0);
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++) {
+        Console.Write(array[i] + " ");
+    }
 
-//     {
-//         evenNumbersSum = evenNumbersSum + currentValue;
-//         evenNumbersCount++;
-//     }
-// else
-//     {
-//          oddNumbersSum = oddNumbersSum + currentValue;
-//          oddNumbersCount++;
-//     }
-// return
-// }
-// Console.WriteLine("Количество четных чисел: " + evenNumbersCount);
-// Console.WriteLine("Количество нечетных чисел: " + oddNumbersCount);
-// Console.WriteLine("Сумма четных чисел: " + evenNumbersSum);
-// Console.WriteLine("Сумма нечетных чисел: " + oddNumbersSum);
+    Console.WriteLine();
+}
+
+void CalculateEven(int[] array) {
+    int evenCount = 0;
+    for (int i = 0; i < array.Length; i++) {
+        if (array[i] % 2 == 0) {
+            evenCount++;
+        }
+    }
+
+    Console.WriteLine("количество четных: " + evenCount);
+}
 
 
 
