f9f94bd49385fd05a47719ab885aee4351c25015
Елена Григорьева <elenagrigoreva@MacBook-Pro-Elena.local>
2022-08-26 17:57:11 +0300

    Решение задачи 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными числами. Напишите программу, которая покажет количество четных 
+// чисел в массиве. 
+// [345, 897, 568, 234] -> 2
+
+
+// int[] numbers = new int[10];
+// FillArrayRandomNumbres(numbers);
+// PrintArray(numbers);
+// int oddNumbersCount = 0;
+// int evenNumbersCount = 0;
+// // int limit = int.Parse(console.ReadLine());
+
+// void FillArrayRandomNumbres(int[] array)
+// {
+// for (int i = 0; i < array.Length; i++)
+// {
+// array[i] = new Random().Next(100, 1000);
+// }
+// }
+
+// void PrintArray(int[] array)
+// {
+// for(int i = 0; i < array.Length; i++)
+// {
+// Console.Write(array[i] + " ");
+// }
+// Console.WriteLine();
+// }
+// // while (array[i] < array.Length)
+// // while (array[i] <= limit)
+// {
+// if (evenNumbersCount % 2 == 0);
+
+//     {
+//         evenNumbersSum = evenNumbersSum + currentValue;
+//         evenNumbersCount++;
+//     }
+// else
+//     {
+//          oddNumbersSum = oddNumbersSum + currentValue;
+//          oddNumbersCount++;
+//     }
+// return
+// }
+// Console.WriteLine("Количество четных чисел: " + evenNumbersCount);
+// Console.WriteLine("Количество нечетных чисел: " + oddNumbersCount);
+// Console.WriteLine("Сумма четных чисел: " + evenNumbersSum);
+// Console.WriteLine("Сумма нечетных чисел: " + oddNumbersSum);
+
+
+
